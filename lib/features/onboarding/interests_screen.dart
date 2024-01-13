afec55f109be90ba93ed268119776310d2543bc1
decembaek <tmdrbpp123@gmail.com>
2024-01-13 15:31:55 +0900

    Wrap 등 반복문을 이용한 UI

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,5 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+
+const interests = [
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+];
 
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
@@ -10,9 +52,98 @@ class InterestsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Choose your interests"),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: Sizes.size24,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v28,
+              const Text(
+                "Choose your interests",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              Gaps.v24,
+              const Text(
+                "Get better video recommendations",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              Gaps.v52,
+              // Wrap 은 children 을 가로로 정렬하는데
+              // 사이즈가 넘어가면 아래줄로 넘어감
+              Wrap(
+                runSpacing: 20,
+                spacing: 20,
+                children: [
+                  for (var interest in interests)
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size16,
+                        horizontal: Sizes.size24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(Sizes.size32),
+                        border:
+                            Border.all(color: Colors.black.withOpacity(0.1)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.05),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        interest,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: Sizes.size96,
+        elevation: 1,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            // bottom: Sizes.size80,
+            top: Sizes.size16,
+            left: Sizes.size24,
+            right: Sizes.size24,
+          ),
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(
+                // vertical: Sizes.size10,
+                // horizontal: Sizes.size12,
+                ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+            child: const Text(
+              "Next",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
         ),
       ),
     );
