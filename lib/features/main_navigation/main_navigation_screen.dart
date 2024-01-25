b7ecfbe5d86e38188dd2324717473c4345ab90b6
decembaek <tmdrbpp123@gmail.com>
2024-01-25 12:20:21 +0900

    bottom navigationBar cupertino style

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -37,66 +38,83 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTap,
-        destinations: const [
-          NavigationDestination(
-            icon: FaIcon(
-              FontAwesomeIcons.house,
-              color: Colors.teal,
-            ),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            // icon: FaIcon(FontAwesomeIcons.house),
+            icon: Icon(CupertinoIcons.house),
             label: "Home",
           ),
-          NavigationDestination(
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-              color: Colors.amber,
-            ),
+          BottomNavigationBarItem(
+            // icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            icon: Icon(CupertinoIcons.search),
             label: "Search",
           ),
         ],
       ),
-      // bottomNavigationBar 아래에 선택 네비게이션
-      // Type을 정할 수 있음
-      // bottomNavigationBar: BottomNavigationBar(
-      //   // 선택시 색상
-      //   selectedItemColor: Theme.of(context).primaryColor,
-      //   // unselectedItemColor: Color,
-      //   // 현재 페이지 인덱스
-      //   currentIndex: _selectedIndex,
-      //   onTap: _onTap,
-      //   items: const [
-      //     BottomNavigationBarItem(
-      //         icon: FaIcon(FontAwesomeIcons.house),
-      //         label: "Home",
-      //         tooltip: "Hello World",
-      //         backgroundColor: Colors.amber),
-      //     BottomNavigationBarItem(
-      //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-      //         label: "Search",
-      //         tooltip: "Hello World",
-      //         backgroundColor: Colors.blue),
-      //     BottomNavigationBarItem(
-      //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-      //         label: "Search",
-      //         tooltip: "Hello World",
-      //         backgroundColor: Colors.red),
-      //     BottomNavigationBarItem(
-      //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-      //         label: "Search",
-      //         tooltip: "Hello World",
-      //         backgroundColor: Colors.black),
-      //     BottomNavigationBarItem(
-      //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-      //         label: "Search",
-      //         tooltip: "Hello World",
-      //         backgroundColor: Colors.green),
-      //   ],
-      // ),
+      tabBuilder: (context, index) => screens[index],
     );
+    // return Scaffold(
+    //   body: screens[_selectedIndex],
+    //   bottomNavigationBar: NavigationBar(
+    //     labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+    //     selectedIndex: _selectedIndex,
+    //     onDestinationSelected: _onTap,
+    //     destinations: const [
+    //       NavigationDestination(
+    //         icon: FaIcon(
+    //           FontAwesomeIcons.house,
+    //           color: Colors.teal,
+    //         ),
+    //         label: "Home",
+    //       ),
+    //       NavigationDestination(
+    //         icon: FaIcon(
+    //           FontAwesomeIcons.magnifyingGlass,
+    //           color: Colors.amber,
+    //         ),
+    //         label: "Search",
+    //       ),
+    //     ],
+    //   ),
+    //   // bottomNavigationBar 아래에 선택 네비게이션
+    //   // Type을 정할 수 있음
+    //   // bottomNavigationBar: BottomNavigationBar(
+    //   //   // 선택시 색상
+    //   //   selectedItemColor: Theme.of(context).primaryColor,
+    //   //   // unselectedItemColor: Color,
+    //   //   // 현재 페이지 인덱스
+    //   //   currentIndex: _selectedIndex,
+    //   //   onTap: _onTap,
+    //   //   items: const [
+    //   //     BottomNavigationBarItem(
+    //   //         icon: FaIcon(FontAwesomeIcons.house),
+    //   //         label: "Home",
+    //   //         tooltip: "Hello World",
+    //   //         backgroundColor: Colors.amber),
+    //   //     BottomNavigationBarItem(
+    //   //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //   //         label: "Search",
+    //   //         tooltip: "Hello World",
+    //   //         backgroundColor: Colors.blue),
+    //   //     BottomNavigationBarItem(
+    //   //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //   //         label: "Search",
+    //   //         tooltip: "Hello World",
+    //   //         backgroundColor: Colors.red),
+    //   //     BottomNavigationBarItem(
+    //   //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //   //         label: "Search",
+    //   //         tooltip: "Hello World",
+    //   //         backgroundColor: Colors.black),
+    //   //     BottomNavigationBarItem(
+    //   //         icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+    //   //         label: "Search",
+    //   //         tooltip: "Hello World",
+    //   //         backgroundColor: Colors.green),
+    //   //   ],
+    //   // ),
+    // );
   }
 }
