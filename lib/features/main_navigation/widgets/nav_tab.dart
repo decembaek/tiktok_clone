fd0a17abaa10f21c2bb179d1d612e2186d301fe2
tmdrbpp123 <tmdrbpp123@gbit.kr>
2024-01-26 14:40:00 +0900

    custom navigation var

---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  const NavTab({
+    super.key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.onTap,
+  });
+
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded 사용 공간을 최대로 쓰기
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            duration: const Duration(microseconds: 300),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              // Column 자리 차지하는 크기 설정하기
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  // FontAwesomeIcons.house,
+                  icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
