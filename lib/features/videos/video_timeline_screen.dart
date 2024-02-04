1618b9900f50010c85d2fb52bf28a5a2c7c1890e
decembaek <tmdrbpp123@gmail.com>
2024-02-04 18:00:19 +0900

    pageController, videoController add

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimeLineScreen extends StatefulWidget {
   const VideoTimeLineScreen({super.key});
@@ -9,27 +10,89 @@ class VideoTimeLineScreen extends StatefulWidget {
 }
 
 class _VideoTimeLineScreenState extends State<VideoTimeLineScreen> {
+  int _itemCount = 4;
+
+  final PageController _pageController = PageController();
+
+  final _scrollDuration = const Duration(milliseconds: 250);
+  final _scrollCurve = Curves.linear;
+
+  // List<Color> colors = [
+  //   Colors.blue,
+  //   Colors.red,
+  //   Colors.yellow,
+  //   Colors.teal,
+  // ];
+
+  void _onPageChanged(int page) {
+    // page controller
+    _pageController.animateToPage(
+      page,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      // colors.addAll([
+      //   Colors.blue,
+      //   Colors.red,
+      //   Colors.yellow,
+      //   Colors.teal,
+      // ]);
+      setState(() {});
+    }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // PageView 스크롤 기능
-    return PageView(
+    return PageView.builder(
+      controller: _pageController,
       // snapping bool
-      // 4 m, 44 second
       pageSnapping: true,
-      children: [
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.amber,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-      ],
+      // scrollDirection 비디오 스크롤 방향
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
+        // color: colors[index],
+        // child: Center(
+        //   child: Text(
+        //     "Screen $index",
+        //     style: const TextStyle(
+        //       fontSize: 68,
+        //     ),
+        //   ),
+        // ),
+      ),
+      // children: [
+      //   Container(
+      //     color: Colors.blue,
+      //   ),
+      //   Container(
+      //     color: Colors.red,
+      //   ),
+      //   Container(
+      //     color: Colors.amber,
+      //   ),
+      //   Container(
+      //     color: Colors.green,
+      //   ),
+      // ],
     );
   }
 }
